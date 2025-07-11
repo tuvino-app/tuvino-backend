a95061ec168811d2df276ddfcf3c81b8e26de7e0
Adrian <are@fi.uba.ar>
2025-07-11 17:52:45 -0300

    Modelo de preferencias

---
+++ src/models/preference.py
@@ -0,0 +1,19 @@
+from src.models.preference_category import PreferenceCategory
+class Preference:
+    id: int
+    category: PreferenceCategory | None
+    option: str
+    description: str
+
+    def __init__(self, id: int, option: str, description: str):
+        self.id = id
+        self.option = option
+        self.description = description
+
+    def set_category(self, category):
+        self.category = category
+
+    def get_option_and_description(self):
+        if self.description is None:
+            return self.option
+        return f"{self.option} - {self.description}"
