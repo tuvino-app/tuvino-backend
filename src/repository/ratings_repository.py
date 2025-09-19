7f7a44e85fc62cc23b57af4f95f13df26e2956fc
Adrian <are@fi.uba.ar>
2025-09-19 17:36:03 -0300

    Edit rating if user has already rated the wine

--- src/repository/ratings_repository.py
+++ src/repository/ratings_repository.py
@@ -11,11 +11,23 @@ class WineRatingsRepository(BaseRepository):
     def __init__(self, session: Session):
         super().__init__(session)
 
+    def get_by_user_id_and_wine_id(self, user_id: str, wine_id: int):
+        return self.session.execute(
+            select(WineRatingModel).where(
+                WineRatingModel.user_id == user_id,
+                WineRatingModel.wine_id == wine_id
+            )
+        ).scalars().first()
+
     def save(self, rating: Rating):
-        self.session.add(WineRatingModel(
-            user_id=str(rating.user_id),
-            wine_id=rating.wine_id,
-            rating=rating.rating,
-        ))
+        original_rating = self.get_by_user_id_and_wine_id(str(rating.user_id), rating.wine_id)
+        if original_rating:
+            original_rating.rating = rating.rating
+        else:
+            self.session.add(WineRatingModel(
+                user_id=str(rating.user_id),
+                wine_id=rating.wine_id,
+                rating=rating.rating,
+            ))
 
         return self.session.commit()
